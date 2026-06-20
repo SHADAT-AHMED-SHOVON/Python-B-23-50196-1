cfceee0b3faed0d5b5cc184ade36ba7c9458e7b4
SHADAT-AHMED-SHOVON <shadatashovon16@gmail.com>
2026-06-20 20:23:58 +0600

    Lab 1

--- Lab_1/Task_3.py
+++ Lab_1/Task_3.py
@@ -1,1 +1,14 @@
-print("Shovon")
+# taking input , mathematical operations, output
+
+x = int(input("Enter X value: "))
+y = int(input("Enter y value: "))
+
+sum = x + y
+sub = x - y
+mult = x * y
+div = x / y 
+
+print("Summaton: ", sum)
+print("Subtraction: ", sub)
+print("Multiplicatoin: ", mult)
+print("Division: ", round(div,2))
